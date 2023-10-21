2cdbbecac962a80d353dabaa9828981fdf441cfe
Kiokosvm <sammysticalfrisk@gmail.com>
2023-10-21 12:34:17 +0300

    Add variable names,global variables and multiple value variables files

---
+++ variable-names.py
@@ -0,0 +1,25 @@
+# A variable can have a short name like x or y or a more descriptive name (age,total_volume,carname)
+#Rules for python variables:
+#  -A variable name must start with a letter or the underscore character
+#  -A variable name cannot start with a number
+#  -A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#  -Variable names are case-sensitive (age, Age and AGE are three different variables)
+#  -A variable name cannot be any of the Python keywords.
+
+myvar = "John"
+my_var = "Sam"
+_my_var = "Lexx"
+myVar = "Joy"
+MYVAR = "Jilo"
+myvar2 = "Miki"
+
+#There are several techniques you can use to make variable names with more than one word more readable:
+
+#Camel Case
+myVariableName = "John"
+#Pascal Case
+MyVariableName = "John"
+#Snake Case
+my_variable_name = "John"
+
+
