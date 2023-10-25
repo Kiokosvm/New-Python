9ac2b0eb1077a832177f7cadfd009a21b5f5d659
Kiokosvm <sammysticalfrisk@gmail.com>
2023-10-25 14:35:46 +0300

    Add type conversion

---
+++ type-conversion.py
@@ -0,0 +1,28 @@
+#You can convert from one type to another with the int(), float(), and complex() methods:
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Python has a built-in module called random that can be used to make random numbers:
+
+import random
+
+print(random.randrange(1, 10)) #Import the random module, and display a random number between 1 and 9:
